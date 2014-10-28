b3aaf19a3a4a6bb759d52d0ee9e94045e6c55829
jverzani <jverzani@gmail.com>
2014-10-28 07:51:33 -0400

    consistent return type of real_roots

--- src/real_roots.jl
+++ src/real_roots.jl
@@ -130,12 +130,12 @@ end
 
 ## find real_roots of a polynomial
 ## Not exported. Call via fzeros(p::Poly) or fzeros(f::Function)
-##
+## return array of roots
 function real_roots(p::Poly)
     if Roots.degree(p) <= 0
-        p[0] == 0.0 ? 0.0 : error("constant polynomial is non-zero")
+        p[0] == 0.0 ? [0.0] : error("constant polynomial is non-zero")
     elseif Roots.degree(p) == 1
-        -p[0]/p[1]
+        [-p[0]/p[1]]
     else
         ## need square free
         m, u, v, w = initial_gcd_system(p)
