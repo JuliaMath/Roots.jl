ceb1903fef3c4c4d5f1f2148ac72db0fde67689a
jverzani <jverzani@gmail.com>
2018-08-30 14:43:11 -0400

    adjust tolerance to have proper type

--- test/test_find_zeros.jl
+++ test/test_find_zeros.jl
@@ -111,3 +111,12 @@ end
     @test length(xrts) >= 3           # should be 5
     @test all(azero.((f7,), xrts))
 end
+
+
+@testset "not Float64 types" begin
+    f(x) = cos(x) - x/10
+    for T in [Float16, Float32, BigFloat]
+        rts = find_zeros(f, T(0.0), T(10.0))
+        @test eltype(rts) == T
+    end
+end
