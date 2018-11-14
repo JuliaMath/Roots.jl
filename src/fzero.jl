d2dc3513c469e950b4e98188d24e891e8d7e11e9
jverzani <jverzani@gmail.com>
2018-11-14 10:26:38 -0500

    doc adjustments

--- src/fzero.jl
+++ src/fzero.jl
@@ -7,13 +7,56 @@
 
 """
     fzero(f, x0; order=0; kwargs...)
+    fzero(f, x0, M; kwargs...)
+    fzero(f, x0, M, N; kwargs...)
+    fzero(f, x0; kwargs...)
+    fzero(f, a::Number, b::Numbers; kwargs...)
+    fzero(f, a::Number, b::Numbers; order=?, kwargs...)
+    fzero(f, fp, a::Number; kwargs...)
 
-Find zero of a function using an iterative algorithm
+Find zero of a function using one of several iterative algorithms.
 
 * `f`: a scalar function or callable object
-* `x0`: an initial guess, finite valued.
+
+* `x0`: an initial guess, a scalar value or tuple of two values
+
 * `order`: An integer, symbol, or string indicating the algorithm to
-   use. The default is `Order0`.
+   use for `find_zero`. The `Order0` default may be specified directly
+   by `order=0`, `order=:0`, or `order="0"`; `Order1()` by `order=1`,
+   `order=:1`, `order="1"`, or `order=:secant`; `Order1B()` by
+   `order="1B", etc.
+
+* `M`: a specific method, as would be passed to `find_zero`, bypassing
+  the use of the `order` keyword
+
+* `N`: a specific bracketing method. When given, if a bracket is
+  identified, method `N` will be used to finish instead of method `M`.
+
+* `a`, `b`: When two values are passed along, if no `order` value is
+  specified, `Bisection` will be used over the bracketing interval
+  `(a,b)`. If an `order` value is specified, the value of `x0` will be set to
+  `(a,b)` and the specified method will be used.
+
+* `fp`: when `fp` is specified (assumed to compute the derivative of `f`),
+  Newton's method will be used
+
+* `kwargs...`: See `find_zero` for the specification of tolerances and other keyword arguments
+
+Examples:
+```
+fzero(sin, 3)                  # use Order0() method, the default
+fzero(sin, 3, order=:secant)   # use secant method (also just `order=1`)
+fzero(sin, 3, Roots.Order1B()) # use secant method variant for multiple roots.
+fzero(sin, 3, 4)               # use bisection method over (3,4)
+fzero(sin, 3, 4, xatol=1e-6)   # use bisection method until |x_n - x_{n-1}| <= 1e-6
+fzero(sin, 3, 3.1, order=1)    # use secant method with x_0=3.0, x_1 = 3.1
+fzero(sin, (3, 3.1), order=2)  # use Steffensen's method with x_0=3.0, x_1 = 3.1
+fzero(sin, cos, 3)             # use Newton's method
+```
+
+Note: unlike `find_zero`, `fzero` does not specialize on the type of
+the function argument. This has the advantage of making the first use
+of the function `f` faster, but subsequent uses slower.
 
 """
 function fzero(f, x0::Number; kwargs...)
@@ -22,10 +65,6 @@ function fzero(f, x0::Number; kwargs...)
     derivative_free(f, x; kwargs...)
 end
 
-
-"""
-    fzero(f, x0, M, [N]; kwargs...)
-"""
 function fzero(f, x0, M::AbstractUnivariateZeroMethod; kwargs...)
     find_zero(FnWrapper(f), x0, M; kwargs...)
 end
@@ -34,14 +73,6 @@ function fzero(f, x0, M::AbstractUnivariateZeroMethod, N::AbstractBracketing; kw
     find_zero(FnWrapper(f), x0, M, N; kwargs...)
 end
 
-"""
-    fzero(f, a, b; kwargs...)
-
-If `order` is not specified, will use bisection to find a zero of a
-function within a bracket, [a,b]. Otherwise, will use `x0=(a,b)` for
-the method specified by `order`.
-
-"""
 function fzero(f, bracket::Tuple{T,S}; kwargs...)  where {T <: Number, S<:Number}
     if haskey(kwargs, :order)
         find_zero(FnWrapper(f), bracket, _method_lookup[kwargs[:order]]; kwargs...)
@@ -54,16 +85,6 @@ fzero(f, a::Number, b::Number; kwargs...) = fzero(f, (a,b); kwargs...)
 fzero(f, bracket::Vector{T}; kwargs...)  where {T <: Number} = fzero(f,(bracket[1],bracket[2]); kwargs...)
 
 
-
-
-"""
-    fzero(f, fp, x0; kwargs)
-
-Find zero using Newton's method.
-
-Dispatches to `find_zero((f,fp), x0, Roots.Newton(); kwargs...)`.
-
-"""
 fzero(f::Function, fp::Function, x0::Real; kwargs...) = find_zero((f,fp), x0, Newton(); kwargs...)
 
 
@@ -79,15 +100,19 @@ _method_lookup = Dict(0   => Order0(),
                       :1   => Order1(),
                       "1"  => Order1(),
                       :secant => Order1(),
+                      :Secant => Order1(),
                       "1B" => Order1B(),
                       :king => Order1B(),
+                      :King => Order1B(),
 
                      2    => Order2(),
                      :2   => Order2(),
                      :steffensen   => Order2(),
+                     :Steffensen   => Order2(),
                      "2"  => Order2(),
                      "2B" => Order2B(),
                      :esser   => Order2B(),
+                     :Esser   => Order2B(),
 
                       5  => Order5(),
                      :5  => Order5(),
