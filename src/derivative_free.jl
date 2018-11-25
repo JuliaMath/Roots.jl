69d7c996a681608a7a505a19efa74e6151c2c185
jverzani <jverzani@gmail.com>
2018-11-25 12:27:27 -0500

    edits

--- src/derivative_free.jl
+++ src/derivative_free.jl
@@ -691,7 +691,7 @@ end
 
 ##################################################
 ## some means of guarding against large fx when taking a secant step
-function steff_step(M::Union{Order5, Order8, Order16}, x, fx)
+function steff_step(M::Union{Order5, Order8, Order16}, x::S, fx::T) where {S, T}
 
     xbar, fxbar = real(x/oneunit(x)), fx/oneunit(fx)
     thresh =  max(1, abs(xbar)) * sqrt(eps(one(xbar))) #^(1/2) # max(1, sqrt(abs(x/fx))) * 1e-6
