44bd1152cf7b034bfdb6a158a707dc3d43b48ff3
jverzani <jverzani@gmail.com>
2022-04-01 14:10:41 -0400

    fix error seen in lith docs

--- src/convergence.jl
+++ src/convergence.jl
@@ -203,7 +203,7 @@ function assess_convergence(M::Any, state::AbstractUnivariateZeroState, options)
 end
 
 # speeds up exact f values by just a bit (2% or so) over the above, so guess this is worth it.
-function assess_convergence(M::Any, state::AbstractUnivariateZeroState, options::Union{ExactOptions,FExactOptions})
+function assess_convergence(M::AbstractBracketingMethod, state::AbstractUnivariateZeroState, options::Union{ExactOptions,FExactOptions})
 
 #    isnan_f(M, state) && return (:nan, true)
 #    is_exact_zero_f(M, state, options) &&  return (:exact_zero, true)
