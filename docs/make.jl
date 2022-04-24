619e1dfefc5fa25a9a6c1d10f34426b09ac106f9
hyrodium <hyrodium@gmail.com>
2022-04-24 12:45:38 +0900

    enable documentation deployment

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,6 @@ makedocs(
     modules = [Roots]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/JuliaMath/Roots.jl"
+)
