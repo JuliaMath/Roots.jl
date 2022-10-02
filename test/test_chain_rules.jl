8e77e5c009715dea7041d324b478c6f8741ecf41
David Widmann <david.widmann@it.uu.se>
2022-10-02 23:39:31 +0200

    Add some inference checks with `ZygoteRuleConfig`

--- test/test_chain_rules.jl
+++ test/test_chain_rules.jl
@@ -6,12 +6,15 @@ using Test
 # issue #325 add frule, rrule
 
 @testset "Test frule and rrule" begin
-    # Type inference tests of `test_frule` and `test_rrule` fail currently
+    # Type inference tests of `test_frule` and `test_rrule` with the default
+    # rule config `ChainRulesTestUtils.TestConfig()` fail due to an issue
+    # with ChainRulesTestUtils: https://github.com/JuliaDiff/ChainRulesTestUtils.jl/issues/246
 
     # single function
     f(x, p) = log(x) - p
     test_frule(solve, ZeroProblem(f, 1), Order1(), 1.0; check_inferred=false)
     test_rrule(solve, ZeroProblem(f, 1), Order1(), 1.0; check_inferred=false)
+    test_rrule(Zygote.ZygoteRuleConfig(), solve, ZeroProblem(f, 1), Order1(), 1.0)
     F(p) = find_zero(f, 1, Order1(), p)
     @test first(Zygote.gradient(F, 1)) ≈ exp(1)
 
@@ -25,6 +28,7 @@ using Test
     fx(x, p) = 1 / x
     test_frule(solve, ZeroProblem((f, fx), 1), Roots.Newton(), 1.0; check_inferred=false)
     test_rrule(solve, ZeroProblem((f, fx), 1), Roots.Newton(), 1.0; check_inferred=false)
+    test_rrule(Zygote.ZygoteRuleConfig(), solve, ZeroProblem((f, fx), 1), Roots.Newton(), 1.0)
     F2(p) = find_zero((f, fx), 1, Roots.Newton(), p)
     @test first(Zygote.gradient(F2, 1)) ≈ exp(1)
 
