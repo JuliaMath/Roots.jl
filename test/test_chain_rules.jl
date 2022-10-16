33decb7d016d2ec8a880586ffeeaf387c9c35225
jverzani <jverzani@users.noreply.github.com>
2022-10-16 01:20:19 +0000

    Format .jl files [skip ci]

--- test/test_chain_rules.jl
+++ test/test_chain_rules.jl
@@ -31,14 +31,32 @@ using Test
     test_frule(solve, ZeroProblem((f, fx), 1), Roots.Newton(), 1.0; check_inferred=false)
     test_rrule(solve, ZeroProblem((f, fx), 1), Roots.Newton(), 1.0; check_inferred=false)
     if isdefined(Zygote, :ZygoteRuleConfig)
-        test_rrule(Zygote.ZygoteRuleConfig(), solve, ZeroProblem((f, fx), 1), Roots.Newton(), 1.0)
+        test_rrule(
+            Zygote.ZygoteRuleConfig(),
+            solve,
+            ZeroProblem((f, fx), 1),
+            Roots.Newton(),
+            1.0,
+        )
     end
     F2(p) = find_zero((f, fx), 1, Roots.Newton(), p)
     @test first(Zygote.gradient(F2, 1)) ≈ exp(1)
 
     gx(x, p) = 2x - p[1]
-    test_frule(solve, ZeroProblem((g, gx), 1), Roots.Newton(), [0.0, 4.0]; check_inferred=false)
-    test_rrule(solve, ZeroProblem((g, gx), 1), Roots.Newton(), [0.0, 4.0]; check_inferred=false)
+    test_frule(
+        solve,
+        ZeroProblem((g, gx), 1),
+        Roots.Newton(),
+        [0.0, 4.0];
+        check_inferred=false,
+    )
+    test_rrule(
+        solve,
+        ZeroProblem((g, gx), 1),
+        Roots.Newton(),
+        [0.0, 4.0];
+        check_inferred=false,
+    )
     G2(p) = find_zero((g, gx), 1, Roots.Newton(), p)
     @test first(Zygote.gradient(G2, [0, 4])) ≈ [1 / 2, 1 / 4]
 end
