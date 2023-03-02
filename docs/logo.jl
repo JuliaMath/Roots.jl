c0cb2baa98cb36cbf7814609b0b73c6abd5c3d8e
john verzani <jverzani@gmail.com>
2023-03-02 14:40:38 -0500

    transparent bg (#351)

--- docs/logo.jl
+++ docs/logo.jl
@@ -5,7 +5,9 @@ f(x) =  x^5 - x - 1/4
 a, b = -9/8, 5/4
 zs = find_zeros(f, (a,b))
 
-p = plot(f, a, b; color=:black, linewidth=2, grid=false, legend=false, showaxis = false)
+p = plot(f, a, b; color=:black,
+         linewidth=2, grid=false, legend=false, showaxis = false,
+         background_color = :transparent)
 plot!(p, zero, color=julia_colors[1])
 scatter!(p, zs, zero.(zs), color=julia_colors[2:4], markersize=10)
 save("src/assets/logo.png", p)
