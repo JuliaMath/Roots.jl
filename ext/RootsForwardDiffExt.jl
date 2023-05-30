194df7c1cc8babbfb23d4fe245df33ae777c50c8
jverzani <jverzani@gmail.com>
2023-05-30 10:53:27 -0400

    add comment about what is 1.9 (julia)

--- ext/RootsForwardDiffExt.jl
+++ ext/RootsForwardDiffExt.jl
@@ -1,3 +1,4 @@
+
 module RootsForwardDiffExt
 
 using Roots
@@ -5,21 +6,26 @@ using ForwardDiff
 import ForwardDiff: Dual, value, partials
 
 # For ForwardDiff we add a `solve` method for Dual types
+# TODO (Issue #384) ForwardDiff.hessian fails, but this works:
+#function hess(f, p)
+#    ∇(p) = ForwardDiff.gradient(f, p)
+#    ForwardDiff.jacobian(∇, p)
+#end
+
 function Roots.solve(ZP::ZeroProblem,
-               M::Roots.AbstractUnivariateZeroMethod,
-               𝐩::Union{Dual{T},
+                     M::Roots.AbstractUnivariateZeroMethod,
+                     𝐩::Union{Dual{T},
                         AbstractArray{<:Dual{T,<:Real}}
                         };
-               kwargs...) where {T}
+                     kwargs...) where {T}
+
     f = ZP.F
     pᵥ = value.(𝐩)
-
     xᵅ = solve(ZP, M, pᵥ; kwargs...)
     𝐱ᵅ = Dual{T}(xᵅ, one(xᵅ))
 
     fₓ = partials(f(𝐱ᵅ, pᵥ), 1)
     fₚ = partials(f(xᵅ, 𝐩))
-
     Dual{T}(xᵅ, - fₚ / fₓ)
 end
 
