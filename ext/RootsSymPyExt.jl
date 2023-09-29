2e7c255e859a88238e5f858f180c8623babe2921
john verzani <jverzani@gmail.com>
2023-09-29 09:23:36 -0400

    Symbolic extensions (#401)
    * widen applicability of equation syntax

--- ext/RootsSymPyExt.jl
+++ ext/RootsSymPyExt.jl
@@ -11,5 +11,20 @@ function Roots.Callable_Function(M::Roots.AbstractUnivariateZeroMethod, f::SymPy
     Roots.Callable_Function(M, lambdify(f), p)
 end
 
+function Roots.FnWrapper(f::SymPy.Sym)
+    if f.is_Equality == true
+        f = lhs(f) - rhs(f)
+    end
+    Roots.FnWrapper(lambdify(f))
+end
+
+
+## allow find_zeros to use symbolic equation
+function Roots.find_zeros(f::SymPy.Sym, a, b=nothing; kwargs...)
+    if f.is_Equality == true
+        f = lhs(f) - rhs(f)
+    end
+    find_zeros(lambdify(f), a, b; kwargs...)
+end
 
 end
--- ext/RootsSymPyPythonCallExt.jl
+++ ext/RootsSymPyPythonCallExt.jl
@@ -11,5 +11,21 @@ function Roots.Callable_Function(M::Roots.AbstractUnivariateZeroMethod, f::SymPy
     Roots.Callable_Function(M, lambdify(f), p)
 end
 
+function Roots.FnWrapper(f::SymPyPythonCall.Sym)
+    if f.is_Equality == true
+        f = lhs(f) - rhs(f)
+    end
+    Roots.FnWrapper(lambdify(f))
+end
+
+
+## allow find_zeros to use symbolic equation
+function Roots.find_zeros(f::SymPyPythonCall.Sym, a, b=nothing; kwargs...)
+    if f.is_Equality == true
+        f = lhs(f) - rhs(f)
+    end
+    find_zeros(lambdify(f), a, b; kwargs...)
+end
+
 
 end
