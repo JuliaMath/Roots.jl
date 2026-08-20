21be2ac5c64830cc118e27adef3510996befc134
jverzani <jverzani@gmail.com>
2026-08-20 14:56:09 -0400

    add Mullers method

---
+++ src/DerivativeFree/muller.jl
@@ -0,0 +1,102 @@
+"""
+    Muller()
+
+*Muller’s method* generalizes the secant method, but uses quadratic
+interpolation among three points instead of linear interpolation between two.
+Solving for the zeros of the quadratic allows the method to find complex
+pairs of roots.
+
+Given three previous guesses for the root `xᵢ₋₂`, `xᵢ₋₁`, `xᵢ`, and the values
+of the polynomial `f` at those points, the next approximation `xᵢ₊₁` is produced.
+
+Excerpt and the algorithm taken from W.H. Press, S.A. Teukolsky, W.T. Vetterling and B.P. Flannery *Numerical Recipes in C*, Cambridge University Press (2002), p. 371.
+
+
+Note that the method may return a complex result even for real initial values
+as this depends on the function.
+
+"""
+struct Muller <: AbstractSecantMethod end
+initial_fncall(::Muller) = 3
+
+struct MullerState{T,S} <: AbstractUnivariateZeroState{T,S}
+    xn2::T
+    xn1::T
+    xn0::T
+    fxn2::S
+    fxn1::S
+    fxn0::S
+end
+
+function init_state(M::Muller, F::Callable_Function, x)
+    # can specify 1, 2 or 3 initial points
+    x′ = float.(x)
+
+    n = length(x′)
+
+    if isa(x′, Number) || n == 2
+        x0, x1 = x₀x₁(x′)
+        fx0, fx1 = F(x0), F(x1)
+        x2 = x1 + fx1 * (x1-x0)/(fx1-fx0)
+        fx2 = F(x2)
+    elseif n > 2 # take last 3
+        xs = x0, x1, x2 = x′[end-2], x′[end-1], x′[end]
+        fx0, fx1, fx2 = F.(xs)
+    elseif n == 1
+        throw(ArgumentError("Specify x as a numer or colection of two or more values"))
+    end
+
+
+    state = MullerState(x2, x1, x0, fx2, fx1, fx0)
+end
+
+function update_state(
+    M::Muller,
+    F,
+    o::MullerState{T,S},
+    options,
+    l=NullTracks(),
+) where {T,S}
+
+    a, b, c = o.xn0, o.xn1, o.xn2
+    fa, fb, fc = o.fxn0, o.fxn1, o.fxn2
+
+    q = (c - b) / (b - a)
+    q² = q^2
+    q1 = q + one(q)
+
+    A = q * fc - q * q1 * fb + q² * fa
+    B = (q1 + q) * fc - q1^2 * fb + q² * fa
+    C = q1 * fc
+
+    Δ = B^2 - 4A * C
+    if typeof(Δ) <: Real && Δ < 0 &&
+            throw(
+                DomainError(
+                    Δ,
+                    "Discriminant is negative and the function most likely has #complex roots. You might want to call muller with complex input.",
+                ),
+            )
+    end
+
+    Δ = √Δ
+    d⁺ = B + Δ
+    d⁻ = B - Δ
+    den = abs(d⁺) > abs(d⁻) ? d⁺ : d⁻
+
+    x =  c - (c - b) * 2C / den
+    fx = first(F(x))
+
+    # check for oddity?
+
+    incfn(l)
+    @reset o.xn0 = b
+    @reset o.xn1 = c
+    @reset o.xn2 = x
+    @reset o.fxn0 = fb
+    @reset o.fxn1 = fc
+    @reset o.fxn2 = fx
+
+
+    return (o, false)
+end
